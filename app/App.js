7dd493fde66f57894ea061ce05231285e5df3185
markus <datamastery87@gmail.com>
2022-08-21 08:28:20 +0200

    update app

--- app/App.js
+++ app/App.js
@@ -1,33 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import { TouchableWithoutFeedback } from "react-native-web";
+import { StyleSheet, Text, ImageBackground, SafeAreaView } from "react-native";
+import Header from "./components/Header";
+import Items from "./components/Items";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useState, useEffect, useCallback } from "react";
 
 export default function App() {
+  // https://docs.expo.dev/versions/latest/sdk/splash-screen/
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          caveat: require("./fonts/OpenSans-Medium.ttf"),
+          "caveat-bold": require("./fonts/OpenSans-Bold.ttf"),
+          "caveat-semibold": require("./fonts/OpenSans-SemiBold.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.alignContainer}>
-        <TextInput placeholder="Todo hinzufügen..."></TextInput>
-        <Button title="Add Todo" color="#841584"></Button>
-      </View>
-
-      <Text>This is my awesome app</Text>
-
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <ImageBackground
+        source={require("./assets/shopping.jpg")}
+        style={styles.container}
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+      >
+        <Header>ToDo-Liste</Header>
+        <Items></Items>
+        <StatusBar style="auto" />
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "gray",
   },
-  alignContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 10,
-    width: "100%",
+  backgroundImage: {
+    opacity: 0.25,
   },
 });
